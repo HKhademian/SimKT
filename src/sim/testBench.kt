a7f1b9edfe83ce4841144626f2932b61df735655
Hossain Khademian <hco@post.com>
2020-07-16 18:54:52 +0430

    success check dFlipFlop by coroutines

--- src/sim/testBench.kt
+++ src/sim/testBench.kt
@@ -1,24 +1,50 @@
 package sim
 
-import sim.base.ConstValue
-import sim.base.Value
-import sim.complex.mux2
-import sim.gates.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import sim.base.VariableValue
+import sim.complex.DFlipFlop
+
+//fun main() {
+//  val A = ConstValue(true)
+//  val B = ConstValue(false)
+//  val C = ConstValue(false)
+//
+//  val res = andg(
+//
+//	org(A.not, B, C),
+//	notg(B),
+//	xorg(B, A)
+//
+//  )
+//
+//  println("res: " + res.get())
+//
+//  println("res mux: " + mux2(Value.ONE, A, B))
+//}
 
 fun main() {
-  val A = ConstValue(true)
-  val B = ConstValue(false)
-  val C = ConstValue(false)
+  val data = VariableValue(false)
+  val clock = VariableValue(false)
+  val dff = DFlipFlop(data, clock)
 
-  val res = andg(
+  runBlocking {
 
-	org(A.not, B, C),
-	notg(B),
-	xorg(B, A)
+	async {
+	  while (true) {
+		clock.set(!clock.get())
+		println("clock tick")
+		delay(1000)
+	  }
+	}
 
-  )
+	while (true) {
+	  val value = dff.eval().get()
+	  data.set(!value)
+	  println(value)
+	  delay(100)
+	}
 
-  println("res: " + res.get())
-
-  println("res mux: " + mux2(Value.ONE, A, B))
+  }
 }
