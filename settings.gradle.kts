2531f2bb9c6183d7ef9eed990b68facd47d382a8
Hossain Khademian <hossain.khademian@gmail.com>
2020-07-17 13:08:58 +0430

    fix migration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
 rootProject.name = "SimKT"
+
+pluginManagement {
+	repositories {
+		gradlePluginPortal()
+	}
+	plugins {
+		kotlin("jvm") version "1.3.72"
+	}
+}
+
---
+++ src/test/kotlin/sim/LibraryTest.kt
@@ -0,0 +1,11 @@
+package sim
+
+import kotlin.test.Test
+
+class LibraryTest {
+	@Test
+	fun testSomeLibraryMethod() {
+		//val classUnderTest = Library()
+		//assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'")
+	}
+}
