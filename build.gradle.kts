a31bce61651fb91e99a3fc2b8b50184d0d8e0895
Hossain Khademian <hossain.khademian@gmail.com>
2020-07-18 23:39:35 +0430

    java14

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,3 +15,21 @@ dependencies {
 	testImplementation(kotlin("test"))
 	testImplementation(kotlin("test-junit"))
 }
+
+//configure<JavaPluginConvention> {
+//	sourceCompatibility = JavaVersion.VERSION_1_8
+//}
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_14
+	targetCompatibility = JavaVersion.VERSION_14
+}
+
+tasks {
+	compileKotlin {
+		kotlinOptions.jvmTarget = "1.8"
+	}
+	compileTestKotlin {
+		kotlinOptions.jvmTarget = "1.8"
+	}
+}
